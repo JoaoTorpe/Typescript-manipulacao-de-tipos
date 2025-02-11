ded4cc52d51592ae1156a0ab9e90b65c44beec49
joaoTorpe <victotorpe10@gmail.com>
2025-02-10 22:23:44 -0300

    refactor: mudando exemplo de URL dinamica

--- literal.ts
+++ literal.ts
@@ -16,14 +16,16 @@ const btn2: ButtonVariant = "btn-huge";   //"huge" não está definido
 
 //Exemplo 01 criando nome para arquivos
 type FileType = "jpg" | "png" | "gif" ;
-type FileName = `image.${FileType}`;
+type FileName = `${string}.${FileType}`;
 
 const file1: FileName = "image.jpg"; // Válido
 const file2: FileName = "image.pdf"; //Erro, 'pdf' não é uma extensão válida
 
 
 //Exemplo 02 url dinâmica
-type ApiEndpoint = `/users/send/${string}`;
+type ApiVersion = "v1" | "v2";
+type ApiEndpoint = `/users/send/${ApiVersion}`;
 
-const endpoint: ApiEndpoint = "/users/send/posts"; //Válido
-const invalidEndpoint: ApiEndpoint = "/users/update/posts"; //corresponde ao formato
+const endpoint: ApiEndpoint = "/users/send/v1"; //Válido
+
+const invalidEndpoint: ApiEndpoint = "/users/send/v3"; //corresponde ao formato
